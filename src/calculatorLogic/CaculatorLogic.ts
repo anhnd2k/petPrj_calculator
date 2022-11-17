571c863e30cd15a8778f5ae3b537f55491d3cf6b
Ngô Hoàng Anh <48317506+anhnd2k@users.noreply.github.com>
2022-11-17 09:47:01 +0700

    resolve logic success

--- src/calculatorLogic/CaculatorLogic.ts
+++ src/calculatorLogic/CaculatorLogic.ts
@@ -1,48 +1,59 @@
 const resolveResult = (data: string): any => {
-	const dataArr = data.split(' ');
-    
-function resolveCalculator(dataArr2){
-    const dataIn = [...dataArr2]
-    // xử lý trong ngoặc
-    for(let i = 0; i < dataIn.length; i++){
-	    if(dataIn[i] === '('){
-	        const findIndexEnd = dataIn.findIndex(item => item === ')')
-	        const newDataClone = [...dataIn]
-	        const spliceSpx = newDataClone.splice(i + 1, findIndexEnd - 1)
-	        const data1 = resolveCalculator(spliceSpx)
-	        dataIn.splice(i, findIndexEnd + 1, data1)
+	function resolveCalculator(dataArr2){
+	    const dataIn = [...dataArr2]
+	    const dataResolver1 = JSON.parse(JSON.stringify(dataIn));
+		for (let i = 0; i < dataResolver1.length; i++) {
+			if (dataResolver1[i] === 'x') {
+				const result = parseFloat(dataResolver1[i - 1]) * parseFloat(dataResolver1[i + 1]);
+				dataResolver1.splice(i - 1, 3, result.toString());
+			}
+			if (dataResolver1[i] === '/') {
+				const result =
+					parseFloat(dataResolver1[i - 1]) /
+					parseFloat(dataResolver1[i + 1]);
+				dataResolver1.splice(i - 1, 3, result);
+			}
+		}
+
+		const newDataResolveFirst = JSON.parse(JSON.stringify(dataResolver1));
+		let total = parseFloat(newDataResolveFirst[0]);
+		for (let j = 1; j < newDataResolveFirst.length; j += 2) {
+		if (newDataResolveFirst[j] === '+') {
+			total = total + parseFloat(newDataResolveFirst[j + 1]);
+		} else {
+			total = total - parseFloat(newDataResolveFirst[j + 1]);
+		}
+		}
+		return total
+	}
+
+	function resolveFirst (arrIn) {
+	    let arr = [...arrIn]
+	    const i = arr.findIndex(item => item === ')')
+	    if(i !== -1){
+		for(let j = i; j >= 0; j--){
+		    if(arr[j] === '('){
+			const arrClone = [...arr]
+			const news = arrClone.splice(j + 1, i - j - 1)
+			const dataResolve = resolveCalculator(news)
+			arr.splice(j, i - j + 1, dataResolve)
+			arr = [...resolveFirst(arr)]
+			break
+		    }
+		}
 	    }
-    }
-    
-    const dataResolver1 = JSON.parse(JSON.stringify(dataIn));
-	for (let i = 0; i < dataResolver1.length; i++) {
-		if (dataResolver1[i] === 'x') {
-			const result = parseFloat(dataResolver1[i - 1]) * parseFloat(dataResolver1[i + 1]);
-			dataResolver1.splice(i - 1, 3, result.toString());
-		}
-		if (dataResolver1[i] === '/') {
-			const result =
-				parseFloat(dataResolver1[i - 1]) /
-				parseFloat(dataResolver1[i + 1]);
-			dataResolver1.splice(i - 1, 3, result);
-		}
+	    return arr
 	}
+	// const data = '2 x 3 + ( 3 x ( 4 - 2 ) ) + 10 - 3'
+	// const data = '( 4 + 5 + ( 4 - 2 + ( 1 + 2 ) ) ) - 6 x ( 2 + 7 )'
+	// const data = '( 4 + 5 + ( 4 - 2 + ( 1 + 2 x ( 1 + 3 x ( 2 + 1 ) ) ) ) ) - 6 x ( 2 + 7 )'
 
-	const newDataResolveFirst = JSON.parse(JSON.stringify(dataResolver1));
-	let total = parseFloat(newDataResolveFirst[0]);
-	for (let j = 1; j < newDataResolveFirst.length; j += 2) {
-        if (newDataResolveFirst[j] === '+') {
-    		total = total + parseFloat(newDataResolveFirst[j + 1]);
-    	} else {
-    		total = total - parseFloat(newDataResolveFirst[j + 1]);
-    	}
-	}
-	return total
-}
+	const arr = data.split(' ')
+	let arrFinal = resolveFirst(arr)
+	const dataResult = resolveCalculator(arrFinal)
+	console.log("==>> ", dataResult)
+	return dataResult
 
-
-const dataTotal = resolveCalculator(dataArr)
-console.log("==>> result", dataTotal)
 };
 
 export default resolveResult;
